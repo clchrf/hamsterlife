db25fca9cfd7bb9c75a3cd12de5044689fc16230
你的名字 <Email>
2025-05-15 08:57:21 +0800

    🔥 強制更新 hamsterlife 網頁

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1714494049"
+    serviceWorkerVersion: "1773591174"
   }
 });
